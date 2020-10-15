d9152aae47a5210bf4dd092b4d519cfdb0b01880
Tu Huynh <tu.huynh@linecorp.com>
2020-10-15 16:15:51 +0700

    add websocket

--- src/main/java/io/oddgame/jodd/Main.java
+++ src/main/java/io/oddgame/jodd/Main.java
@@ -5,6 +5,7 @@ import com.jinyframework.core.AbstractRequestBinder.HttpResponse;
 import io.oddgame.jodd.factories.AppFactory;
 import io.oddgame.jodd.middlewares.JWTMiddleware;
 import io.oddgame.jodd.modules.authenticate.AuthenticateRouter;
+import io.oddgame.jodd.websocket.OddWebSocket;
 import lombok.val;
 
 import java.io.IOException;
@@ -14,8 +15,11 @@ public class Main {
         val gson = AppFactory.getGson();
         val server = HttpServer.port(1234)
                 .useTransformer(gson::toJson);
+        new Thread(OddWebSocket.port(1235)).start();
+
         server.get("/", JWTMiddleware::jwtMiddleware, ctx -> HttpResponse.of(ctx.dataParam("username")));
         server.use("/auth", AuthenticateRouter.getRouter());
+
         server.start();
     }
 }
---
+++ src/main/java/io/oddgame/jodd/websocket/OddWebSocket.java
@@ -0,0 +1,52 @@
+package io.oddgame.jodd.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+@Slf4j
+public class OddWebSocket extends WebSocketServer {
+    private OddWebSocket(final int port) {
+        super(new InetSocketAddress("localhost", port));
+    }
+
+    public static OddWebSocket port(final int port) {
+        return new OddWebSocket(port);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conn.send("Welcome to the server!");
+        broadcast( "New connection: " + handshake.getResourceDescriptor() );
+        log.info("New connection to " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        log.info("Closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        log.info("Received message from " + conn.getRemoteSocketAddress() + ": " + message);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message ) {
+        log.info("Received ByteBuffer from " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.err.println("An error occurred on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
+    }
+
+    @Override
+    public void onStart() {
+        log.info("OddWebSocket started successfully");
+    }
+}
