f25507c1304db9c089704a913fef95ad62f3c380
Tu Huynh <tu.huynh@linecorp.com>
2020-10-17 01:13:24 +0700

    update chat module ++

--- src/main/java/io/oddgame/jodd/websocket/OddWebSocket.java
+++ src/main/java/io/oddgame/jodd/websocket/OddWebSocket.java
@@ -3,6 +3,7 @@ package io.oddgame.jodd.websocket;
 import io.oddgame.jodd.utils.JWTUtils;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import lombok.var;
 import org.java_websocket.WebSocket;
 import org.java_websocket.drafts.Draft;
 import org.java_websocket.exceptions.InvalidDataException;
@@ -12,15 +13,61 @@ import org.java_websocket.handshake.ServerHandshakeBuilder;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class OddWebSocket extends WebSocketServer {
+    private final Map<String, Collection<WebSocket>> rooms = new HashMap<>();
+
     private OddWebSocket(final int port) {
         super(new InetSocketAddress("localhost", port));
     }
 
     public static OddWebSocket port(final int port) {
         return new OddWebSocket(port);
+    }
+
+    private String getConnName(WebSocket conn) {
+        var senderName = "system";
+        if (conn != null) {
+            val attachment = (Attachment) conn.getAttachment();
+            senderName = attachment.getNickname();
+        }
+        return senderName;
+    }
+
+    private void doBroadcast(WebSocket sender, String topic, String text) {
+        val msg = topic + ":" + getConnName(sender) + ":" + text;
+        broadcast(msg);
+    }
+
+    private void doBroadcastRoom(WebSocket sender, String topic, String text, String roomName) {
+        val msg = topic + ":" + getConnName(sender) + ":" + text;
+        broadcast(msg, rooms.get(roomName));
+    }
+
+    private void addToRoom(String roomName, WebSocket webSocket) {
+        val attachment = (Attachment) webSocket.getAttachment();
+        if (attachment.getInRoom() != null && attachment.getInRoom().equals(roomName)) return;
+
+        val r = rooms.get(roomName) == null ? new ArrayList<WebSocket>() : rooms.get(roomName);
+        r.add(webSocket);
+        rooms.put(roomName, r);
+        attachment.setInRoom(roomName);
+        doBroadcastRoom(null, "chat/private", attachment.getNickname() + " joined room ", roomName);
+    }
+
+    private void removeFromRoom(String roomName, WebSocket webSocket) {
+        val attachment = (Attachment) webSocket.getAttachment();
+        if (attachment.getInRoom() != null && attachment.getInRoom().equals(roomName)) {
+            val r = rooms.get(roomName);
+            r.remove(webSocket);
+            attachment.setInRoom(null);
+            doBroadcastRoom(null, "chat/private", attachment.getNickname() + " has left room " + roomName, roomName);
+        }
     }
 
     @Override
@@ -35,10 +82,35 @@ public class OddWebSocket extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        val attachment = (Attachment) conn.getAttachment();
-        val messageToBroadcast = attachment.getNickname() + ": " + message;
-        broadcast(messageToBroadcast);
         log.info("Received message from " + conn.getRemoteSocketAddress() + ": " + message);
+        val attachment = (Attachment) conn.getAttachment();
+
+        val messageArr = message.split(":");
+        if (messageArr.length < 2) {
+            conn.send("Invalid message!");
+            return;
+        }
+        val topic = messageArr[0];
+        val data = messageArr[1];
+        switch (topic) {
+            case "message":
+                if (attachment.getInRoom() == null) {
+                    doBroadcast(conn, "chat/public", data);
+                } else {
+                    val room = attachment.getInRoom();
+                    doBroadcastRoom(conn, "chat/private", data, room);
+                }
+                break;
+            case "room/in":
+                addToRoom(data, conn);
+                break;
+            case "room/out":
+                removeFromRoom(data, conn);
+                break;
+            default:
+                conn.send("Invalid message!");
+                break;
+        }
     }
 
     @Override
